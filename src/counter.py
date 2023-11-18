2f4692fba30dad50012677acbd31400d5800b5b2
GuiCoelhoDev <guiminervino@gmail.com>
2023-11-17 22:54:11 -0300

    REFAC: Melhora código do counter.py

--- src/counter.py
+++ src/counter.py
@@ -5,33 +5,129 @@ from typing import Dict, List, Tuple
 from argparse import ArgumentParser
 
 
-def extract_messages_of_week(input_file_path: str, start_date: datetime, end_date: datetime):
-    """
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input_file_path', type=str,
+                        default='./assets/input/chat.txt')
+    parser.add_argument('-o', '--output_directory_path',
+                        type=str, default='./assets/output')
+    parser.add_argument('-d', '--dia_da_contagem', type=str,
+                        default=datetime.now().strftime('%d/%m/%Y'))
+    args = parser.parse_args()
 
-    :type end_date: datetime
+    input_file_path = args.input_file_path
+    output_directory_path = args.output_directory_path
+    dia_da_contagem = args.dia_da_contagem
+    end_date = datetime.strptime(dia_da_contagem, '%d/%m/%Y')
+    start_date = end_date - timedelta(days=6)
+
+    messages_from_date_interval = extract_messages_from_date_interval(
+        input_file_path, start_date, end_date)
+
+    nucleo_and_points, name_and_points = create_dict_nucleo2points(
+        messages_from_date_interval)
+
+    save_results_file(nucleo_and_points, name_and_points,
+                      output_directory_path, start_date, end_date)
+
+    print(f"Relatório gerado com sucesso e salvo em {output_directory_path}")
+
+
+def extract_messages_from_date_interval(input_file_path: str, start_date: datetime, end_date: datetime) -> List[str]:
+    """
+    :param input_file_path: caminho do arquivo de entrada; ex: './assets/input/chat.txt'
+    :param start_date: data de início da contagem; ex: datetime(2023, 7, 10) # 2023-07-10 00:00:00
+    :param end_date: data de fim da contagem; ex: datetime(2023, 7, 16) # 2023-07-16 00:00:00
+    :return messages_from_date_interval: lista de mensagens do grupo focafit entre start_date e end_date
     """
     with open(input_file_path, 'r', encoding='utf-8') as file:
-        text = file.readlines()
+        messages = file.readlines()
 
-    messages = []
-    for line in text:
+    messages_from_date_interval = []
 
-        # padrão para identificar o dia da mensagem
-        match = re.search(r'\d{2}/\d{2}/\d{4}', line)
-        if match:
-            message_date = datetime.strptime(match.group(0), '%d/%m/%Y')
+    # para cada linha das mensagens do grupo focafit, fazer:
+    for message in messages:
+        # REFAC: Em vez de usar for para cada mensagem, usar regex para filtrar todas as mensagens de uma só vez
+        # identificar a data contida na linha
+        date_match = re.search(r'\d{2}/\d{2}/\d{4}', message)
+        if date_match:
+            message_date = datetime.strptime(date_match.group(0), '%d/%m/%Y')  # 2023-04-04 00:00:00
             if start_date <= message_date <= end_date:
-                messages.append(line)
+                messages_from_date_interval.append(message)
 
-    return messages
+    return messages_from_date_interval
 
-def get_capitalized_name(message_text: str):
-    capitalized_words = re.findall(
-            r'\b[A-Z][^A-Z ]+\b', message_text)  # ex: Chico Buarque
-    name = ' '.join(capitalized_words)
 
-    return name
+def create_dict_nucleo2points(messages_from_date_interval: List) -> Tuple[Dict, Dict]:
+    """
+    :param messages_from_date_interval: lista de mensagens do grupo focafit entre start_date e end_date
+    :return nucleo_and_points: dicionário que mapeia cada nucleo para a sua pontuação
+    :return name_and_points: dicionário que mapeia cada nome para a sua pontuação
+    """
 
+    nucleo_and_points = {}
+    name_and_points = {}
+
+    nucleo_pattern = re.compile(r'#(noe|nip|nut|bope|ndp|pres|trainees)', re.IGNORECASE)  # ex: #noe, #NUT
+    points_pattern = re.compile(r'([\+|\-]\d+)')  # ex: +2, -10
+
+    for message in messages_from_date_interval:
+
+        # Pega o corpo da mensagem (que inicia depois dos dois pontos)
+        # message = '10/05/2023 17:02 - Chico: Chico +2 #NOE'
+        message_body = "".join(message.split(':')[2:]).strip()  # NOTE: pq não por [-1]?
+
+        nucleo_match = nucleo_pattern.search(message_body)  # ex: #noe
+        points_match = points_pattern.search(message_body)  # ex: +2, -10
+
+        if nucleo_match and points_match:
+            message_without_nucleo = nucleo_pattern.sub('', message_body)  # removes #noe
+            cleaned_message = points_pattern.sub('', message_without_nucleo).strip()  # removes +2
+
+            nucleo = nucleo_match.group(1).lower()
+            points = points_match.group(1)
+
+            assign_points(nucleo_and_points, name_and_points, message, cleaned_message, nucleo, points)
+        else:
+            continue
+
+    return nucleo_and_points, name_and_points
+
+
+# sub function
+def assign_points(nucleo_and_points: Dict, name_and_points: Dict, message: str, message_text: str, nucleo: str,
+                  points: str):
+    names = []
+
+    # caso message_text possua nucleo e points mas não tenha nome, nucleo pontua mas a pessoa não.
+    # pontuação para o núcleo será considerada mas não será atribuída a nenhum jogador
+
+    # pontuando o núcleo
+    if nucleo in nucleo_and_points:
+        nucleo_and_points[nucleo] += int(points)
+    else:
+        nucleo_and_points[nucleo] = int(points)
+    try:
+        if ',' in message_text:
+            names = get_names_from_message(message_text)
+            points = int(points) // len(names)
+        else:
+            name = get_name_from_message(message_text)
+            if name:
+                names.append(name)
+
+        if not names:
+            print(f'Mensagem sem nome: {message}')
+
+        for name in names:
+            if name in name_and_points:
+                name_and_points[name] += int(points)
+            else:
+                name_and_points[name] = int(points)
+    except ZeroDivisionError:
+        print(f'Problema ao pegar nomes e pontos na seguinte mensagem: {message}')
+
+# sub function
 def get_name_from_message(message_text: str):
     # Se só tiver uma palavra, essa palavra é o nome
     message_words = message_text.split(' ')
@@ -46,6 +142,8 @@ def get_name_from_message(message_text: str):
 
     return name.title()
 
+
+# sub function
 def get_names_from_message(message_text: str):
     names = []
     splitted_message = message_text.split(',')
@@ -58,67 +156,18 @@ def get_names_from_message(message_text: str):
     return names
 
 
-def assign_points(sector_and_points: Dict, name_and_points: Dict, message: str, message_text: str, sector: str, points: str):
-    names = []
+# sub function
+def get_capitalized_name(message_text: str):
+    capitalized_words = re.findall(
+        r'\b[A-Z][^A-Z ]+\b', message_text)  # ex: Chico Buarque
+    name = ' '.join(capitalized_words)
 
-    # caso message_text possua sector e points mas não tenha nome, sector pontua mas a pessoa não.
-    # pontuação para o núcleo será considerada mas não será atribuída a nenhum jogador
-    if sector in sector_and_points:
-        sector_and_points[sector] += int(points)
-    else:
-        sector_and_points[sector] = int(points)
-
-    if ',' in message_text:
-        names = get_names_from_message(message_text)
-        points = int(points) // len(names)
-    else:
-        name = get_name_from_message(message_text)
-        if name:
-            names.append(name)
-
-    if not names:
-        print(f'Mensagem sem nome: {message}')
-
-    for name in names:
-        if name in name_and_points:
-            name_and_points[name] += int(points)
-        else:
-            name_and_points[name] = int(points)
+    return name
 
 
-def create_dict_sector2points(messages: List) -> Tuple[Dict, Dict]:
-    sector_and_points = {}
-    name_and_points = {}
-
-    sector_pattern = re.compile(r'#(noe|nip|nut|bope|ndp|pres|trainees)', re.IGNORECASE)
-    points_pattern = re.compile(r'([\+|\-]\d+)') # ex: +2, -10
-
-    for message in messages:
-
-        # Pega o corpo da mensagem (que inicia depois dos dois pontos)
-        # message = '10/05/2023 17:02 - Chico: Chico +2 #NOE'
-        message_text = "".join(message.split(':')[2:]).strip()
-
-        sector_match = sector_pattern.search(message_text)  # ex: #noe
-        points_match = points_pattern.search(message_text)  # ex: +2, -10
-
-        if sector_match and points_match:
-            message_without_sector = sector_pattern.sub('', message_text)  # removes #noe
-            cleaned_message = points_pattern.sub('', message_without_sector).strip()  # removes +2
-
-            sector = sector_match.group(1).lower()
-            points = points_match.group(1)
-
-            assign_points(sector_and_points, name_and_points, message, cleaned_message, sector, points)
-        else:
-            continue
-
-    return sector_and_points, name_and_points
-
-
-def save_results_file(sector_and_points: Dict, name_and_points, output_directory_path: str, start_date: datetime,
+def save_results_file(nucleo_and_points: Dict, name_and_points, output_directory_path: str, start_date: datetime,
                       end_date: datetime):
-    sector_and_points_sorted_by_points = dict(sorted(sector_and_points.items(),
+    nucleo_and_points_sorted_by_points = dict(sorted(nucleo_and_points.items(),
                                                      key=lambda item: item[1],
                                                      reverse=True))
 
@@ -137,12 +186,12 @@ def save_results_file(sector_and_points: Dict, name_and_points, output_directory
             f'🦾 FOCA FIT SEMANAL - {start_date_formatted} A {end_date_formatted} 🦾 \n')
         file.write('Gerado por: Focafit_pointser 😎 \n\n')
 
-        file.write('💜💙🖤 RANKING POR NÚCLEO 💚🧡💛 \n\n')
+        file.write('💜💙🖤✅ RANKING POR NÚCLEO ✅💚🧡💛 \n\n')
 
-        for rank, sector_points in enumerate(sector_and_points_sorted_by_points.items()):
-            sector = sector_points[0]
-            points = sector_points[1]
-            file.write(f'{rank + 1}º {sector.upper()}: {points}\n')
+        for rank, nucleo_points in enumerate(nucleo_and_points_sorted_by_points.items()):
+            nucleo = nucleo_points[0]
+            points = nucleo_points[1]
+            file.write(f'{rank + 1}º {nucleo.upper()}: {points}\n')
 
         file.write('\n\n')
 
@@ -154,33 +203,4 @@ def save_results_file(sector_and_points: Dict, name_and_points, output_directory
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-i', '--input_file_path', type=str,
-                        default='./assets/input/chat.txt')
-    parser.add_argument('-o', '--output_directory_path',
-                        type=str, default='./assets/output')
-    parser.add_argument('-d', '--dia_da_contagem', type=str,
-                        default=datetime.now().strftime('%d/%m/%Y'))
-    args = parser.parse_args()
-
-    input_file_path = args.input_file_path
-    output_directory_path = args.output_directory_path
-    dia_da_contagem = args.dia_da_contagem
-    today = datetime.strptime(dia_da_contagem, '%d/%m/%Y')
-
-    # considerando que a contagem se inicia na segunda-feira
-    start_date = today - timedelta(days=7)
-
-    # considerando que o relatório de contagem é realizado na próxima segunda (um dia depois do último dia válido para a contagem da semana em questão)
-    end_date = today - timedelta(days=1)
-
-    messages_of_week = extract_messages_of_week(
-        input_file_path, start_date, end_date)
-
-    sector_and_points, name_and_points = create_dict_sector2points(
-        messages_of_week)
-
-    save_results_file(sector_and_points, name_and_points,
-                      output_directory_path, start_date, end_date)
-
-    print(f"Relatório gerado com sucesso e salvo em {output_directory_path}")
+    main()
